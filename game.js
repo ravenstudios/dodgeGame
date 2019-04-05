55ab72332bea8ddcb25c7ce37c91109fdd6a2635
ravenstudios <ravenstudiosrob@yahoo.com>
2019-04-04 20:39:29 -0500

    init commit

---
+++ game.js
@@ -0,0 +1,25 @@
+var gameWidth = 600;
+var gameHeight = 400;
+
+
+
+
+function setup(){
+	createCanvas(gameWidth, gameHeight);
+
+}
+
+
+function update(){
+
+
+}
+
+
+function draw(){
+	background(51);
+
+	fill(255, 0, 255);
+	rect(100, 100, 50, 50);
+	update();
+}
